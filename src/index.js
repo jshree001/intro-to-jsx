bc7644244e56f6c64fdbafc94587c0e3a60fa45f
jshree001 <142464439+jshree001@users.noreply.github.com>
2023-08-16 20:00:17 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Chips</li>
+      <li>Cakes</li>
+      <li>Ice Cream</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
